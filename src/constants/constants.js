5c95a858cb08c2ed4ff0162806ace14ace2d832f
Kaspars Upenieks <36580099+Kaskadieris@users.noreply.github.com>
2021-12-06 20:13:16 +0200

    added random-quotes project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,64 +1,79 @@
 export const projects = [
   {
-    title: 'FreeCodeCamp.org- Responsive Web Design',
-    description: 'All 5 of freecodecamp.org "Responsive Web Design" course projets. Finished course in order to refresh all  basic HTML5/CSS3 skills and pass my  freecodecamp.org "Responsive Web Design" certification',
-      image: '/images/1.jpg',
-      tags: ['HTML5', 'CSS3'],
-    source: 'https://github.com/Kaskadieris/Portfolio_webpage',
-    visit: 'https://mystifying-jang-b387e8.netlify.app/',
+    title: "FreeCodeCamp.org- Responsive Web Design",
+    description:
+      'All 5 of freecodecamp.org "Responsive Web Design" course projets. Finished course in order to refresh all  basic HTML5/CSS3 skills and pass my  freecodecamp.org "Responsive Web Design" certification',
+    image: "/images/1.jpg",
+    tags: ["HTML5", "CSS3"],
+    source: "https://github.com/Kaskadieris/Portfolio_webpage",
+    visit: "https://mystifying-jang-b387e8.netlify.app/",
     id: 0,
   },
- 
+
   {
-    title: 'FreeCodeCamp.org- JavaScript Algorithms and Data Structures',
-    description:'All 5 of freecodecamp.org "JavaScript Algorithms and Data Structures" course projets,created in graphical interface. Finished course in order to refresh all  basic JavaScript skills and pass my  freecodecamp.org "JavaScript Algorithms and Data Structures" certification',
-    image: '/images/2.jpg',
-    tags: ['HTML5', 'CSS3', 'JavaScript'],
-    source: 'https://github.com/Kaskadieris/Javascript_cert',
-    visit: 'https://serene-sinoussi-16afa5.netlify.app/',
+    title: "FreeCodeCamp.org- JavaScript Algorithms and Data Structures",
+    description:
+      'All 5 of freecodecamp.org "JavaScript Algorithms and Data Structures" course projets,created in graphical interface. Finished course in order to refresh all  basic JavaScript skills and pass my  freecodecamp.org "JavaScript Algorithms and Data Structures" certification',
+    image: "/images/2.jpg",
+    tags: ["HTML5", "CSS3", "JavaScript"],
+    source: "https://github.com/Kaskadieris/Javascript_cert",
+    visit: "https://serene-sinoussi-16afa5.netlify.app/",
     id: 1,
   },
 
-   
   {
-    title: 'Frontend practise-Ableton',
-    description: "Design that I recreated from https://www.frontendpractice.com  to practise HTML and CSS3 concepts, learned from courses.",
-      image: '/images/3.jpg',
-      tags: ['HTML5', 'CSS'],
-    source: 'https://github.com/Kaskadieris/FrontedPractise-1',
-    visit: 'https://angry-spence-f2ffd8.netlify.app/',
+    title: "Frontend practise-Ableton",
+    description:
+      "Design that I recreated from https://www.frontendpractice.com  to practise HTML and CSS3 concepts, learned from courses.",
+    image: "/images/3.jpg",
+    tags: ["HTML5", "CSS"],
+    source: "https://github.com/Kaskadieris/FrontedPractise-1",
+    visit: "https://angry-spence-f2ffd8.netlify.app/",
     id: 2,
   },
 
-  /*
   {
-    title: 'Project 4',
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet ligula felis. Nam urna mi, aliquam ac diam consequat, ultrices porta ex. Donec tristique velit ac porttitor porta. Sed eleifend.n",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "Random quote generator",
+    description:
+      "Generates random quotes from https://type.fit/api/quotes. Created using React,JavaScript,Bootstrap,SASS/CSS and HTML",
+    image: "/images/4.jpg",
+    tags: ["React", "HTML5", "SASS/CSS", "Bootstrap", "JavaScript"],
+    source: "https://github.com/Kaskadieris/random_quotes",
+    visit: "https://thirsty-goodall-a5b2f4.netlify.app/",
     id: 3,
   },
-  */
 ];
 
 export const TimeLineData = [
-  { year: 2013, text: 'Started my journey', },
-  { year: 2015, text: 'Got First professional degree in computer programming', },
-  { year: 2015, text: 'Started working as intern, then as programmer/etl developer', },
-  { year: 2019, text: 'Got my Bachelors degree in computer science', },
-  { year: 2020, text: 'Continue working as Programmer/ETL Developer', },
-  { year: 2021, text: 'Started to improve and develope my skill more into web development area', },
-  { year: 2021, text: 'FreeCodeCamp.org "Responsive Web Design" certification', },
-  { year: 2021, text: 'FreeCodeCamp.org "JavaScript Algorithms and Data Structures" certification', },
+  { year: 2013, text: "Started my journey" },
+  { year: 2015, text: "Got First professional degree in computer programming" },
+  {
+    year: 2015,
+    text: "Started working as intern, then as programmer/etl developer",
+  },
+  { year: 2019, text: "Got my Bachelors degree in computer science" },
+  { year: 2020, text: "Continue working as Programmer/ETL Developer" },
+  {
+    year: 2021,
+    text: "Started to improve and develope my skill more into web development area",
+  },
+  {
+    year: 2021,
+    text: 'FreeCodeCamp.org "Responsive Web Design" certification',
+  },
+  {
+    year: 2021,
+    text: 'FreeCodeCamp.org "JavaScript Algorithms and Data Structures" certification',
+  },
 ];
 
 export const AcomplishmentsData = [
-  { text: 'e-Guardion and ECDL Computer skills sertificate BDA',},
-  { text: 'JAVA SE 8 Fundamentals Diploma from Oracle University',},
-  { text: 'Java SE 8 Fundamentals Ed 1 Sertificate',},
-  { text: 'Code Institutes 5 day coding challenge certificate',},
-  { text: 'FreeCodeCamp.org "Responsive Web Design" certification',},
-  { text: 'FreeCodeCamp.org "JavaScript Algorithms and Data Structures" certification',},
+  { text: "e-Guardion and ECDL Computer skills certificate BDA" },
+  { text: "JAVA SE 8 Fundamentals Diploma from Oracle University" },
+  { text: "Java SE 8 Fundamentals Ed 1 Sertificate" },
+  { text: "Code Institutes 5 day coding challenge certificate" },
+  { text: 'FreeCodeCamp.org "Responsive Web Design" certification' },
+  {
+    text: 'FreeCodeCamp.org "JavaScript Algorithms and Data Structures" certification',
+  },
 ];
