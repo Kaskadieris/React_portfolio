fba497b68a6710092bce159de0caf3ccc88da40f
Kaspars Upenieks <36580099+Kaskadieris@users.noreply.github.com>
2021-12-12 15:07:28 +0200

    added technologies in front-end

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { DiFirebase, DiJava, DiPython, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiJava, DiPython, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
+const Technologies = () => (
   <Section id="tech">
     <SectionDivider />
     <br />
@@ -14,24 +25,29 @@ const Technologies = () =>  (
     </SectionText>
     <List>
       <ListItem>
-        <DiReact size="3rem"/>
+        <DiReact size="3rem" />
         <ListContainer>
           <ListTitle>Front-End</ListTitle>
-          <ListParagraph>Eperience with HTML5,CSS3,JavaScript and React.js</ListParagraph>
+          <ListParagraph>
+            Eperience with HTML5,CSS3,JavaScript,TypeScript,SASS, Bootstrap and
+            React.js
+          </ListParagraph>
         </ListContainer>
       </ListItem>
-           <ListItem>
-        <DiFirebase size="3rem"/>
+      <ListItem>
+        <DiFirebase size="3rem" />
         <ListContainer>
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>Eperience with Node.js and Databases</ListParagraph>
         </ListContainer>
       </ListItem>
-           <ListItem>
-        <DiJava size="3rem"/>
+      <ListItem>
+        <DiJava size="3rem" />
         <ListContainer>
           <ListTitle>Programming</ListTitle>
-          <ListParagraph>Eperience with Java,Python, C++ and scripting languages.</ListParagraph>
+          <ListParagraph>
+            Eperience with Java,Python, C++ and scripting languages.
+          </ListParagraph>
         </ListContainer>
       </ListItem>
     </List>
